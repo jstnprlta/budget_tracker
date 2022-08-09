346a90d7d226cf41ac815713feb94f00c4438ef6
Justin Peralta <jstnprlta90@gmail.com>
2022-08-09 01:50:16 -0500

    updates

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,75 @@
+let db;
+const request = indexedDB.open("budget_tacker", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run checkDatabase() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+
+  // add record to your store with add method.
+  budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+  //open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  //access your object store
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+
+  //get all records from store and set to variable
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    //if there was data in indexedDb's store, send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          //open one more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          //access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore("new_transaction");
+          //clear all items in your store
+          budgetObjectStore.clear();
+
+          alert("All saved transactions has been submitted");
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", uploadTransaction);
